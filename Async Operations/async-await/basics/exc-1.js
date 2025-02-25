af942abdea575dddd0149573e84b6f486da49ee0
Blaze-18 <110927484+Blaze-18@users.noreply.github.com>
2025-02-26 02:15:49 +0600

    Delete Async Operations directory

--- Async Operations/async-await/basics/exc-1.js
+++
@@ -1,8 +0,0 @@
-//Write an async function that returns a resolved Promise with the string 
-// "Hello, World!". Use .then() to log the result.
-
-async function helloWorld() {
-    return "Hello, World!";
-}
-  
-helloWorld().then((result) => console.log(result));
